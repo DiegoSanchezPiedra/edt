35580ca6302c7e0698ae00dee0edab01b63ddfe0
isx2031424 <isx2031424@i05.informatica.escoladeltreball.org>
2020-11-27 10:05:54 +0100

    m06 practica pam-ldap

---
+++ 2ASIX/M06/PAM/pam20_ldap/startup.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+#crear els usuaris locals
+useradd unix01
+echo "unix01" | passwd --stdin unix01
+useradd unix02
+echo "unix02" | passwd --stdin unix02
+useradd unix03
+echo "unix03" | passwd --stdin unix03
+
+#fer la configuració dels usuaris LDAP
+cp ldap.conf /etc/openldap/ldap.conf
+cp nslcd.conf /etc/nslcd.conf
+cp nsswitch.conf /etc/nsswitch.conf
+cp system-auth /etc/pam.d/system-auth
+
+#engegar els dimonos nscd i nslcd
+/sbin/nscd
+/sbin/nslcd
+
+/bin/bash
