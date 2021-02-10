028d1e0ae75ff190cde515c0e03b7050d48c2958
didi <didi@localhost.localdomain>
2021-02-10 22:36:55 +0100

    m10 practica_2

--- 2ASIX/M10/practica_2/funcio_corretgit.sql
+++ 2ASIX/M10/practica_2/funcio_corretgit.sql
@@ -28,10 +28,24 @@ BEGIN
              ''',''' || res_sql1.c_postal || ''',''' || res_sql1.telefon || ''',''' || res_sql1.email || ''',''' || res_sql1.nie ||''')';
 
             raise notice '% no existeix',res_sql1.idpacient;
-        --ELSE
+        ELSE
+            sql4 := 'update pacients set
+            idpacient=' || res_sql1.idpacient || '
+            , nom=''' || res_sql1.nom || '''
+            , cognoms=''' || res_sql1.cognoms || '''
+            , data_naix=''' || res_sql1.data_naix || '''
+            , sexe=''' || res_sql1.sexe || '''
+            , adreca=''' || res_sql1.adreca || '''
+            , ciutat=''' || res_sql1.ciutat || '''
+            , c_postal=''' || res_sql1.c_postal || '''
+            , telefon=''' || res_sql1.telefon || '''
+            , email=''' || res_sql1.email || '''
+            , nie=''' || res_sql1.nie || '''';
 
+            raise notice '% si existeix UPDATE' ,res_sql1.idpacient;
         END IF;
-        raise notice  'sentencia: %',sql3;
+        raise notice 'sentencia insert: %',sql3;
+        raise notice 'sentencia update: %',sql4;
     
     END LOOP;
     RETURN 1;
@@ -44,3 +58,20 @@ sql3 := 'insert into pacients (idpacient,nom,cognom,dni,data_naix,sexe,adreca,ci
             values (' || res_sql1.idpacient || ',''' || res_sql1.nom || ''',''' || res_sql1.cognoms || ''',''' || res_sql1.data_naix || ''',''' || res_sql1.ciutat ||
              ''',''' || res_sql1.c_postal || ''',''' || res_sql1.telefon || ''',''' || res_sql1.email || ''',''' || res_sql1.num_ss || ''',''' || res_sql1.num_cat ||
              ''',''' || res_sql1.nie || ''',''' || res_sql1.passaport || ''')';
+
+sql4 := 'update pacients set
+            idpacient=' || res_sql1.idpacient || '
+            , nom=' '' || res_sql1.nom || '' '
+            , cognoms=' '' || res_sql1.cognoms || '' '
+            , dni=' '' || res_sql1.dni || '' '
+            , data_naix=' '' || res_sql1.data_naix || '' '
+            , sexe=' '' || res_sql1.sexe || '' '
+            , adreca=' '' || res_sql1.adreca || '' '
+            , ciutat=' '' || res_sql1.ciutat || '' '
+            , c_postal=' '' || res_sql1.c_postal || '' '
+            , telefon=' '' || res_sql1.telefon || '' '
+            , email=' '' || res_sql1.email || '' '
+            , num_ss=' '' || res_sql1.num_ss || '' '
+            , num_cat=' '' || res_sql1.num_cat || '' '
+            , nie=' '' || res_sql1.nie || '' '
+            , passaport=' '' || res_sql1.passaport || '';
